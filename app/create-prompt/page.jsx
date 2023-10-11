fbbd4f2f934b585721e19b4ab02afb3081c221b0
Jascha-Biederstedt <j.biederstedt@gmx.de>
2023-10-11 12:54:11 +0200

    Implement create prompt handler fn

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -13,7 +13,27 @@ const CreatePrompt = () => {
     tag: '',
   });
 
-  const createPrompt = async event => {};
+  const createPrompt = async event => {
+    event.PreventDefault();
+    setSubmitting(true);
+
+    try {
+      const response = await fetch('/api/prompt/new', {
+        method: 'POST',
+        body: JSON.stringify({
+          prompt: post.prompt,
+          tag: post.tag,
+          userId: session?.user.id,
+        }),
+      });
+
+      if (response.ok) router.push('/');
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
 
   return (
     <Form
