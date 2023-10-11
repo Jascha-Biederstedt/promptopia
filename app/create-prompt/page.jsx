3ccdfa3f5f6179399ab01bb62dc312247fca27ae
Jascha-Biederstedt <j.biederstedt@gmx.de>
2023-10-11 12:02:28 +0200

    Add form component

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,6 +1,16 @@
-import React from 'react';
+'use client';
+
+import { useState } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
 const CreatePrompt = () => {
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: '',
+    tag: '',
+  });
+
   return <div>CreatePrompt</div>;
 };
 
