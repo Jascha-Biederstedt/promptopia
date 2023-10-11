64ece35b5caf9b70dec7c5d0c0d5be3a7ddd1865
Jascha-Biederstedt <j.biederstedt@gmx.de>
2023-10-11 11:41:54 +0200

    Add callback to nextauth route

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -6,42 +6,45 @@ import prisma from '../../../db';
 const handler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
+      clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {
-    const sessionUser = await prisma.user.findUnique({
-      where: {
-        email: session.user.email,
-      },
-    });
-
-    session.user.id = sessionUser._id.toString();
-
-    return session;
-  },
-  async signIn({ profile }) {
-    try {
-      const userExists = await prisma.user.findUnique({
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await prisma.user.findUnique({
         where: {
-          email: profile.email,
+          email: session.user.email,
         },
       });
 
-      if (!userExists) {
-        await prisma.user.create({
-          data: {
+      session.user.id = sessionUser.id.toString();
+
+      return session;
+    },
+    async signIn({ profile }) {
+      try {
+        const userExists = await prisma.user.findUnique({
+          where: {
             email: profile.email,
-            name: profile.name.replace(' ', '').toLowerCase(),
-            image: profile.picture,
           },
         });
+
+        if (!userExists) {
+          await prisma.user.create({
+            data: {
+              email: profile.email,
+              name: profile.name.replace(' ', '').toLowerCase(),
+              image: profile.picture,
+            },
+          });
+        }
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
       }
-    } catch (error) {
-      console.log(error);
-      return false;
-    }
+    },
   },
 });
 
