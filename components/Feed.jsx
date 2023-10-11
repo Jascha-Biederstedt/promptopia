68c588c016df4787330913ec4719b302b5fd3ac9
Jascha-Biederstedt <j.biederstedt@gmx.de>
2023-10-11 14:30:59 +0200

    Add feed component

---
+++ components/Feed.jsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+
+const Feed = () => {
+  const [searchText, setSearchText] = useState('');
+
+  const handleSearchChange = event => {};
+
+  return (
+    <section className='feed'>
+      <form className='relative w-full flex-center'>
+        <input
+          type='text'
+          placeholder='Search for a tag or username...'
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className='search_input peer'
+        />
+      </form>
+    </section>
+  );
+};
+
+export default Feed;
