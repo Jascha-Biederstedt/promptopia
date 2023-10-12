0e3f828b6aa6071058c718241d3f007076f8850e
Jascha-Biederstedt <j.biederstedt@gmx.de>
2023-10-12 12:40:18 +0200

    Add get all prompts route

---
+++ app/api/prompt/route.js
@@ -0,0 +1,11 @@
+import prisma from '../../db';
+
+export const GET = async req => {
+  try {
+    const prompts = await prisma.prompt.findMany();
+
+    return new Response(JSON.stringify(prompts), { status: 200 });
+  } catch (error) {
+    return new Response('Failed to fetch prompts', { status: 500 });
+  }
+};
