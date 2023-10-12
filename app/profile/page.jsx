b299e2672e780494f38d1d93d6762d647834a648
Jascha-Biederstedt <j.biederstedt@gmx.de>
2023-10-12 13:40:37 +0200

    Implement fetching user specific posts in profile page

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -4,8 +4,38 @@ import { useState, useEffect } from 'react';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 
-const Profile = () => {
-  return <div>Profile</div>;
+import Profile from '../../components/Profile';
+
+const ProfilePage = () => {
+  const { data: session } = useSession();
+  const [posts, setPosts] = useState([]);
+
+  const handleEdit = () => {};
+
+  const handleDelete = async => {};
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+
+      setPosts(data);
+    };
+
+    if (session?.user.id) fetchPosts();
+  }, []);
+
+  return (
+    <div>
+      <Profile
+        name='My'
+        desc='Welcome to your personalized profile page'
+        data={posts}
+        handleEdit={handleEdit}
+        handleDelete={handleDelete}
+      />
+    </div>
+  );
 };
 
-export default Profile;
+export default ProfilePage;
