a667d7ffd75cfe282bb07266e136c12ef48771ff
Jascha-Biederstedt <j.biederstedt@gmx.de>
2023-10-14 12:12:15 +0200

    Implement search function in feed page

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -7,8 +7,37 @@ import PromptCardList from './PromptCardList';
 const Feed = () => {
   const [searchText, setSearchText] = useState('');
   const [posts, setPosts] = useState([]);
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchedResults, setSearchedResults] = useState([]);
 
-  const handleSearchChange = event => {};
+  const handleSearchChange = event => {
+    clearTimeout(searchTimeout);
+    setSearchText(event.target.value);
+
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filterPrompts(event.target.value);
+        setSearchedResults(searchResult);
+      }, 500)
+    );
+  };
+
+  const handleTagClick = tagName => {
+    setSearchText(tagName);
+
+    const searchResult = filterPrompts(tagName);
+    setSearchedResults(searchResult);
+  };
+
+  const filterPrompts = searchtext => {
+    const regex = new RegExp(searchtext, 'i'); // 'i' flag for case-insensitive search
+    return posts.filter(
+      item =>
+        regex.test(item.author.name) ||
+        regex.test(item.tag) ||
+        regex.test(item.prompt)
+    );
+  };
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -26,7 +55,7 @@ const Feed = () => {
       <form className='relative w-full flex-center'>
         <input
           type='text'
-          placeholder='Search for a tag or username...'
+          placeholder='Search for a prompt, tag or username...'
           value={searchText}
           onChange={handleSearchChange}
           required
@@ -34,7 +63,14 @@ const Feed = () => {
         />
       </form>
 
-      <PromptCardList data={posts} handleTagClick={() => {}} />
+      {searchText ? (
+        <PromptCardList
+          data={searchedResults}
+          handleTagClick={handleTagClick}
+        />
+      ) : (
+        <PromptCardList data={posts} handleTagClick={handleTagClick} />
+      )}
     </section>
   );
 };
